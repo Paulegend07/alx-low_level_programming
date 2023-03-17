a353a9ce7da844c4621d912d4ce4edece0a779f7
Paulegend07 <pauluboko@gmail.com>
2023-03-17 16:32:49 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - A program that that prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char lc;
+
+	for
+	(lc = 'a'; lc <= 'z'; lc++)
+	{
+	putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+	}
