c3766511bc7d4fec49664369bcb2abf079a6fbd9
Paulegend07 <pauluboko@gmail.com>
2023-03-16 21:36:19 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - A program to print a line using printf
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	printf("/"with proper gramma, but the outcome is a piece of art");
+	return (0);
+}
