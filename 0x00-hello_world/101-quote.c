4d86b1ebeb3e865ed9dfee4cdc36c3f1fb48e73d
Paulegend07 <pauluboko@gmail.com>
2023-03-17 01:51:36 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - A program that print a sentence, followed by a new line to the standard error
+ * Return: Always 1(Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful " / " - Dora Korpar, 2015-10-19,\n", 89);
+return (1);
+}
