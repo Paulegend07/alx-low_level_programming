2130e50b5ddf4150f5e880069919cfdac1a7b6b3
Paulegend07 <pauluboko@gmail.com>
2023-03-19 04:05:27 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,10 +10,13 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 15; n++)
+	for (n = 0; n <= 9; n++)
 	{
 	putchar(n);
-	putchar((14) + '0');
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+	putchar(n);
 	}
 	putchar('\n');
 	return (0);
