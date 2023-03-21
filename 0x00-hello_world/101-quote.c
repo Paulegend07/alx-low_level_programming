13ef50070a3c2c9d026e523768a8fc273301e83a
Paulegend07 <pauluboko@gmail.com>
2023-03-21 06:25:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include<unistd.h>
 
 /**
- * main - A program that print a sentence, followed by a new line to the standard error
+ * main - A program that print a sentence to the standard error
  * Return: Always 1(Success)
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
