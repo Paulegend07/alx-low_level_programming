e3e6e98cc550320a264123e85daf031b96388c75
Paulegend07 <pauluboko@gmail.com>
2023-03-21 10:42:11 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - program that print alphabet in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+return (0);
+}
