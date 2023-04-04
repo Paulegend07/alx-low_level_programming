3189b940705b9cb386c3fca5bda1c913ee6d10cd
Paulegend07 <pauluboko@gmail.com>
2023-04-04 00:13:05 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,15 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-	for (i = 0; s[i] != '\0'; i++)
+	int i = 0;
+
+	while (*(s + i) != '\0')
 	{
-	if (s[i] == c)
-	return s + i;
+	if (*(s + i) == c)
+	{
+	return (s + i);
 	}
-	return NULL;
+	i++;
+	}
+	return (NULL);
 }
