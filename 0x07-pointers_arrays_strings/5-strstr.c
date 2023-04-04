1f50b3213b047b8e5c9f919d9ae94f19527f2561
Paulegend07 <pauluboko@gmail.com>
2023-04-04 03:30:16 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,10 +16,12 @@ char *_strstr(char *haystack, char *needle)
 	{
 	for (j = 0; needle[j] != '\0'; j++)
 	{
-	if (haystack[i] == needle[j])
+	if (haystack[i + j] != needle[j])
+	break;
+	}
+	if (needle[j] == '\0')
 	{
 	return (haystack + i);
-	}
 	}
 	}
 	return (NULL);
