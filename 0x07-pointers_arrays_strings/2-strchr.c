b3ab403ce3986b18144818625dc6530a7533fcfc
Paulegend07 <pauluboko@gmail.com>
2023-04-03 23:47:21 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * *_strchr - function that locate a character in a string
+ * @s: string
+ * @c: character
+ *Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+	if (*s == c)
+	return (s);
+	s++;
+	}
+	return (NULL);
+}
