a8ce60cd12fc2a0e541d7186bcd7a02401c610f2
Paulegend07 <pauluboko@gmail.com>
2023-04-17 16:30:23 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure definition of a dog
+ * @name: pointer to character array(string)
+ * @age: integer
+ * @owner: pointer to character array(string)
+ */
+
 struct dog
 {
 	char *name;
